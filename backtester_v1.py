27a9fe1dfeddf240cf441485562dfc48dcec00aa
Pau1Yang <a22067322@gmail.com>
2020-04-09 18:36:55 -0400

    Dev: Set up backtest process

--- backtester_v1.py
+++ backtester_v1.py
@@ -1,4 +1,5 @@
 import configparser
+from strategy import *
 import datetime as dt
 import pandas as pd
 import numpy as np
@@ -6,44 +7,39 @@ import quandl
 import yfinance as yf
 import matplotlib.pyplot as plt
 
+""" Quandl API
+    def API_key(self, source_='Quandal'):
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        return config['{}'.format(source_)]['API_Key']
 
-# Quandl API
-    # def API_key(self, source_='Quandal'):
-    #     config = configparser.ConfigParser()
-    #     config.read('config.ini')
-    #     return config['{}'.format(source_)]['API_Key']
-    #
-    # def warm_up(self, ticker_):
-    #     quandl.ApiConfig.api_key = self.API_key()
-    #     data = quandl.get_table('WIKI/PRICES', ticker=['AAPL', 'MSFT'], qopts={'columns': ['ticker', 'date', 'adj_close']},
-    #                             date={'gte': '2015-12-31', 'lte': '2016-12-31'}, paginate=True)
+    def warm_up(self, ticker_):
+        quandl.ApiConfig.api_key = self.API_key()
+        data = quandl.get_table('WIKI/PRICES', ticker=['AAPL', 'MSFT'], qopts={'columns': ['ticker', 'date', 'adj_close']},
+                                date={'gte': '2015-12-31', 'lte': '2016-12-31'}, paginate=True)
+"""
 
-class Backtester(object):
-    def __init__(self, ticker_, start_, end_, initial_cash_):
-        self.ticker = ticker_
+
+class Backtester(BuyHoldStrategy):
+    def __init__(self, start_, end_, initial_cash_):
+        super().__init__()
         self.start, self.end = start_, end_
         self.initial_cash = initial_cash_
         self.market_value = 0
         self.port_value = self.initial_cash + self.market_value
-
-    def _add_ticker(self, ticker_, start_, end_):
-        self.start = start_.strftime("%Y-%m-%d") if isinstance(start_, pd.Timestamp) else start_
-        self.end = end_.dt.datetime.strftime("%Y-%m-%d") if isinstance(end_, pd.Timestamp) else end_
-        self.ticker.append(ticker_)
+        self.state = None
 
     def set_up_df(self):
-        if isinstance(self.ticker, list):
-            if len(self.ticker) > 1:
-                df = yf.Tickers(self.ticker).history(start=self.start, end=self.end)['Close']
+        self.state = len(self.ticker_trading)
+        if isinstance(self.ticker_trading, list):
+            if len(self.ticker_trading) > 1:
+                df = yf.Tickers(self.ticker_trading).history(start=self.start, end=self.end)['Close']
             else:
-                df = yf.Ticker(self.ticker[0]).history(start=self.start, end=self.end)['Close'].to_frame()
-                df.rename(columns={'Close': '{}'.format(self.ticker[0])}, inplace=True)
+                df = yf.Ticker(self.ticker_trading[0]).history(start=self.start, end=self.end)['Close'].to_frame()
+                df.rename(columns={'Close': '{}'.format(self.ticker_trading[0])}, inplace=True)
             return df
         else:
             return None
-
-    def input_strategy(self):
-        pass
 
     def back_test(self):
         flag = 1
@@ -51,20 +47,36 @@ class Backtester(object):
             df = self.set_up_df()
             iter = df.iterrows()
             for idx, row in iter:
-                print(idx, row)
-                if idx == pd.Timestamp(2018, 1, 8, 0):
-                    self._add_ticker("AAPL", idx+dt.timedelta(days=1), self.end)
-                    break
+                self.trading_rules(idx, row)
 
+                # Handling case that strategy adding new ticker
+                if self.state != len(self.ticker_trading):
+                    self.state = len(self.ticker_trading)
+                    self.start = (idx+dt.timedelta(days=1)).strftime("%Y-%m-%d") if isinstance(idx, pd.Timestamp) else idx
+                    self.end = self.end.dt.datetime.strftime("%Y-%m-%d") if isinstance(self.end, pd.Timestamp) else self.end
+                    break
+                # If iterating to last row, turn the flag to 0 in order to jump out of while loop
                 if (row == df.iloc[-1, :]).all():
                     flag = 0
 
+    def get_backtest_result(self):
+        pass
+
 
 def main():
+    """ 1. Specifying: 1. initial target ticker to trade and
+                       2. Backtest time frame
+                       3. Initial Capital"""
     ticker = ['TLT', 'LQD']
     start, end = '2018-01-01', '2020-01-01'
-    tmp = Backtester(ticker, start, end, 1000)
-    tmp.back_test()
+    initial_capital = 1000
+
+    """ 2. Set up ticker"""
+    strat = Backtester(start, end, initial_capital)
+    strat.set_ticker(ticker, asset_type_="STK")
+
+    """ 3. Start Backtest"""
+    strat.back_test()
 
 
 if __name__ == '__main__':
--- strategy.py
+++ strategy.py
@@ -1,26 +1,65 @@
 import pandas as pd
 import numpy as np
+import datetime as dt
 from asset import *
 import abc
+
+asset_mapping = {"OPT": OptionAsset, "STK": StockAsset}
 
 
 class Strategy(metaclass=abc.ABCMeta):
     def __init__(self):
         self.positions = {}
-        self.asset_trading = []
-        self.total_pnl = 0
-        self.total_t_cost = 0
+        self.ticker_trading = []
+        self.unrealized_pnl = 0
+        self.t_cost = 0
         self.realized_pnl = 0
 
+    @property
+    def asset_trading(self):
+        return list(set([x.type for x in self.ticker_trading]))
+
+    @property
+    def total_pnl(self):
+        return self.unrealized_pnl + self.realized_pnl + self.t_cost
+
     @abc.abstractmethod
-    def trading_rules(self):
+    def trading_rules(self, timestamp_, asset_obj, end_=None):
         pass
 
+    def set_ticker(self, *args, asset_type_=None):
+        target = args[0]
+        if isinstance(target, list):
+            for item in target:
+                # asset = asset_mapping[asset_type_](item)
+                # self.ticker_trading.append(asset)
+                self.ticker_trading.append(item)
+
+        if isinstance(target, str):
+            # asset = asset_mapping[asset_type_](target)
+            # self.ticker_trading.append(asset)
+            self.ticker_trading.append(target)
+
+    def _warm_up(self):
+        pass
+
+    def _add_ticker(self, ticker_):
+        self.ticker_trading.append(ticker_)
+
+
+class BuyHoldStrategy(Strategy):
+    def __init__(self):
+        super().__init__()
+
+    def trading_rules(self, timestamp_, aseet_obj):
+        if timestamp_ == dt.datetime(2018, 1, 8, 0):
+            self._add_ticker("SPY")
 
 
 def main():
-    pass
-
+    tmp = BuyHoldStrategy()
+    tmp.set_ticker("AAPL", asset_type_="STK")
+    print(tmp.ticker_trading)
 
 if __name__ == '__main__':
     main()
